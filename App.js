60f1ffd17e9ea54a9ba838a986dd8ee8ae0ce5c2
BigDaddyTarpon <gambiatarpon@yahoo.co.uk>
2023-12-26 17:57:42 +0000

    uses constraints to avoid clashing with status bar

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView, { Marker, Polyline } from "react-native-maps";
-import getPolylineCoordinates, { getPoisFromMarker } from "./utils/utils";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Constants from 'expo-constants'
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import OptionsForm from "./components/options-form";
@@ -14,24 +13,34 @@ const Tab = createMaterialTopTabNavigator();
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Text>Local Home in App!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
 function SettingsScreen() {
   return (
+   
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    
       <Text>Local Settings in App!</Text>
     </View>
   );
 }
 
+{/* tried to use: <StatusBar style="auto" />  but needed 'constraints' */}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator style={{ marginTop: Constants.statusBarHeight }}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Local Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />

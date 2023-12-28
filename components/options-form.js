e150d3f6923da1654f6798c1a81db07db96e42e9
BigDaddyTarpon <gambiatarpon@yahoo.co.uk>
2023-12-28 16:30:54 +0000

    working and complete entry ready to convert to form and implement verification library

--- components/options-form.js
+++ components/options-form.js
@@ -4,17 +4,24 @@ import {
   StyleSheet,
   Text,
   View,
+  SafeAreaView,
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-import { TextInput } from "react-native-paper";
-import { RadioButton, List } from "react-native-paper";
+import { TextInput, List, SegmentedButtons } from "react-native-paper";
+import { RadioButton } from "react-native-paper";
 
 export default function OptionsForm() {
   const [origin, setOrigin] = useState("");
   const [destination, setDestination] = useState("");
   const [checked, setChecked] = useState("car");
   const [expanded, setExpanded] = useState(false);
+  const [valueAccomodation, setValueAccomodation] = useState("");
+  const [group1, setGroup1] = useState([]);
+  const [group2, setGroup2] = useState([]);
+  const [group3, setGroup3] = useState([]);
+  const [group4, setGroup4] = useState([]);
+
   const handlePress = () => setExpanded(!expanded);
   return (
     <>
@@ -30,9 +37,10 @@ export default function OptionsForm() {
         value={destination}
         onChangeText={(destination) => setDestination(destination)}
       />
-      <List.Accordion
-        title="Click to open/close to select. Selected;"
-        right={(props) => <List.Icon {...props} icon={checked} />}
+      <View >
+      <List.Accordion 
+        title="<-Selected. Open/close options here."
+        left={(props) => <List.Icon {...props} icon={checked} />}
         expanded={expanded}
         onPress={handlePress}
       >
@@ -46,7 +54,124 @@ export default function OptionsForm() {
           onPress={() => setChecked("train")}
           left={(props) => <List.Icon {...props} icon="train" />}
         />
+        <List.Item
+          title="bus/coach"
+          onPress={() => setChecked("bus")}
+          left={(props) => <List.Icon {...props} icon="bus" />}
+        />
+        <List.Item
+          title="bicycle"
+          onPress={() => setChecked("bicycle")}
+          left={(props) => <List.Icon {...props} icon="bicycle" />}
+        />
+        <List.Item
+          title="walk"
+          onPress={() => setChecked("walk")}
+          left={(props) => <List.Icon {...props} icon="walk" />}
+        />
       </List.Accordion>
+      </View >
+      
+      <SafeAreaView style={styles.container}>
+        <SegmentedButtons
+          value={valueAccomodation}
+          onValueChange={setValueAccomodation}
+          buttons={[
+            {
+              value: "",
+              label: "Day trip",
+              showSelectedCheck: true,
+            },
+            {
+              value: "hotel",
+              label: "Hotel",
+              showSelectedCheck: true,
+            },
+            { value: "camping", label: "Camping", showSelectedCheck: true },
+          ]}
+        />
+      </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <SegmentedButtons
+          multiSelect
+          value={group1}
+          onValueChange={setGroup1}
+          buttons={[
+            {
+              value: "Wheel-Chair-Access",
+              label: "Easy Access",
+              showSelectedCheck: true,
+            },
+            {
+              value: "Kids Entertainment",
+              label: "Kids Fun",
+              showSelectedCheck: true,
+            },
+            { value: "shopping", label: "Shopping", showSelectedCheck: true },
+          ]}
+        />
+      </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <SegmentedButtons
+          multiSelect
+          value={group2}
+          onValueChange={setGroup2}
+          buttons={[
+            {
+              value: "parks&nature",
+              label: "Parks/Nature",
+              showSelectedCheck: true,
+            },
+            {
+              value: "hike",
+              label: "Hikes/Walks",
+              showSelectedCheck: true,
+            },
+            { value: "Wildlife", label: "Wildlife", showSelectedCheck: true },
+          ]}
+        />
+      </SafeAreaView>
+
+      <SafeAreaView style={styles.container}>
+        <SegmentedButtons
+          multiSelect
+          value={group3}
+          onValueChange={setGroup3}
+          buttons={[
+            {
+              value: "Museums",
+              label: "Museums",
+              showSelectedCheck: true,
+            },
+            {
+              value: "Heritage",
+              label: "Heritage",
+              showSelectedCheck: true,
+            },
+            { value: "Theatre", label: "Theatre", showSelectedCheck: true },
+          ]}
+        />
+      </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <SegmentedButtons
+          multiSelect
+          value={group4}
+          onValueChange={setGroup4}
+          buttons={[
+            {
+              value: "Theme Parks",
+              label: "Theme Parks",
+              showSelectedCheck: true,
+            },
+            {
+              value: "Sports&Leisure",
+              label: "Sports/Leisure",
+              showSelectedCheck: true,
+            },
+            { value: "Cinema", label: "Cinema", showSelectedCheck: true },
+          ]}
+        />
+      </SafeAreaView>
     </>
   );
 }
@@ -57,6 +182,9 @@ const styles = StyleSheet.create({
 
     alignItems: "center",
     justifyContent: "center",
+  },
+  container2: {
+    flex: 2,
   },
 });
 
@@ -103,16 +231,4 @@ const styles = StyleSheet.create({
   */
 }
 
-{
-  /* 
-<List.Accordion
-        title="please choose one mode of transport"
-        left={props => <List.Icon {...props} icon="folder" />}
-        expanded={expanded}
-        onPress={handlePress}>
-         <List.Item title="car"  /> 
-        <List.Item title="train" />
-      </List.Accordion> 
-      
-  */
-}
+
